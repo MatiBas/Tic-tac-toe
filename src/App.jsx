6f27a72beade1d73b16860114428530a7cc41499
MatiBas <mwbasauri@gmail.com>
2023-01-24 15:22:18 -0300

    Agrego cambios

---
+++ src/App.jsx
@@ -0,0 +1,92 @@
+import { useState } from "react"
+import confetti from "canvas-confetti"
+
+import { Square } from "./components/Square"
+import { TURNS } from "./constants.js"
+import { checkWinnerFrom, checkEndGame } from "./logic/board"
+import { WinnerModal } from "./components/WinnerModal"
+
+function App() {
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
+  })
+  
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
+  
+
+
+  // null es que no hay ganador, false es que hay un empate
+  const [winner, setWinner] = useState(null)
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
+
+  }
+
+  const updateBoard = (index) => {
+    if (board[index] || winner) return
+    const newBoard = [ ... board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+
+    const newTurn = turn == TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+    //guardar aquí la partida
+    window.localStorage.setItem('board', JSON.stringify(newBoard))
+    window.localStorage.setItem('turn', newTurn)
+    //revisar si hay ganador
+    const newWinner = checkWinnerFrom(newBoard)
+    if (newWinner) { confetti()
+      setWinner(newWinner)
+    } else if (checkEndGame(newBoard)) { 
+      setWinner(false)
+    }
+  }
+
+
+  return (
+
+    <main className="board">
+
+      <h1>Tic tac toe</h1>
+      <button onClick={resetGame}>Empezar de nuevo</button>
+      <section className="game">
+        {
+          board.map((square, index) => {
+            return (
+              // Para renderisar una lista de elemento necesitamos usar key con el .map que devuelve un Array
+              <Square
+              key={index}
+              index={index}
+              updateBoard={updateBoard}
+              >
+                {square}
+              </Square>
+
+            )
+          })
+        }
+
+      </section>
+
+      <section className="turn">
+        <Square isSelected={turn == TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn == TURNS.O}>{TURNS.O}</Square>
+      </section>
+
+        
+        <WinnerModal resetGame={resetGame} winner={winner} />
+    </main>
+  )
+}
+
+export default App
